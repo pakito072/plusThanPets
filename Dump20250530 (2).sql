0be5f2ce85840dedc7e6d396e5d71d96f522fbb9
pakito072 <105859870+pakito072@users.noreply.github.com>
2025-05-30 20:51:03 +0200

    security dump

---
+++ Dump20250530 (2).sql
@@ -0,0 +1,153 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: plusthanpets
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `animals`
+--
+
+DROP TABLE IF EXISTS `animals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animals` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` enum('dog','cat') COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `breed` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `gender` enum('male','female') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `image_url` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `adopted_status` tinyint DEFAULT '1',
+  `owner_id` int DEFAULT NULL,
+  `adopted_by` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `owner_id` (`owner_id`),
+  KEY `adopted_by` (`adopted_by`),
+  CONSTRAINT `animals_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `animals_ibfk_2` FOREIGN KEY (`adopted_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animals`
+--
+
+LOCK TABLES `animals` WRITE;
+/*!40000 ALTER TABLE `animals` DISABLE KEYS */;
+INSERT INTO `animals` VALUES (1,'cat','fsdfsdf','fsdfsdfs',12,'male','fdsdfsdfdsf','https://res.cloudinary.com/djwl7si04/image/upload/c_fill,g_auto,h_500,w_500/v1748504401/qrvacplzjwosgorkb4am.jpg',0,5,2,'2025-05-29 07:40:04'),(2,'dog','dasda','dasdas',3,'male','dsadasdasdasdasdasd','https://res.cloudinary.com/djwl7si04/image/upload/c_fill,g_auto,h_500,w_500/v1748544263/jpslao4svjvectrl8l1f.png',0,2,NULL,'2025-05-29 18:44:36'),(5,'dog','fdsf','fdsf',3,'male','fdsfsdfdsfsdfsd','https://res.cloudinary.com/djwl7si04/image/upload/c_fill,g_auto,h_500,w_500/v1748624536/x9dtscefxs3iqkc3iaen.jpg',0,8,NULL,'2025-05-30 17:02:23');
+/*!40000 ALTER TABLE `animals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chat_messages`
+--
+
+DROP TABLE IF EXISTS `chat_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chat_messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `room_id` int NOT NULL,
+  `sender_id` int NOT NULL,
+  `message` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  KEY `sender_id` (`sender_id`),
+  CONSTRAINT `chat_messages_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `chat_rooms` (`id`),
+  CONSTRAINT `chat_messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat_messages`
+--
+
+LOCK TABLES `chat_messages` WRITE;
+/*!40000 ALTER TABLE `chat_messages` DISABLE KEYS */;
+INSERT INTO `chat_messages` VALUES (1,1,2,'hola','2025-05-29 10:29:47'),(2,1,2,'me recuerdass?','2025-05-29 10:52:38'),(3,1,5,'si ,te recuerdo','2025-05-29 10:53:05'),(4,1,5,'como estas?','2025-05-29 10:53:08'),(5,1,5,'esto tiene limite?','2025-05-29 10:53:13'),(6,1,2,'https://res.cloudinary.com/djwl7si04/image/upload/c_fill,g_auto,h_500,w_500/v1748551857/frpv10eouo4hzq49qflv.jpg','2025-05-29 20:50:57'),(7,1,2,'buenas','2025-05-30 03:10:38'),(8,1,2,'que tal','2025-05-30 03:10:57'),(9,1,2,'todo bien?','2025-05-30 03:11:04'),(10,1,2,'espero que si','2025-05-30 03:11:18');
+/*!40000 ALTER TABLE `chat_messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chat_rooms`
+--
+
+DROP TABLE IF EXISTS `chat_rooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chat_rooms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `animal_id` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `animal_id` (`animal_id`),
+  CONSTRAINT `chat_rooms_ibfk_1` FOREIGN KEY (`animal_id`) REFERENCES `animals` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat_rooms`
+--
+
+LOCK TABLES `chat_rooms` WRITE;
+/*!40000 ALTER TABLE `chat_rooms` DISABLE KEYS */;
+INSERT INTO `chat_rooms` VALUES (1,1,'2025-05-29 10:29:47');
+/*!40000 ALTER TABLE `chat_rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `gender` enum('male','female','other','prefer_not_to_say') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role` enum('visitor','gestor','admin') COLLATE utf8mb4_general_ci DEFAULT 'visitor',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'fdsfsdf','fsdfsdf@gmail.com','Pta-18092002','female','visitor','2025-05-28 16:25:17'),(2,'Pizo','pepe@gmail.com','Pta-18092002','female','visitor','2025-05-28 16:31:12'),(3,'fdsfdsf','fdsfsdfsdfsdf@gmail.com','Pta-18092002','prefer_not_to_say','visitor','2025-05-28 16:46:35'),(4,'jkhjkhjkb','khkjhkjhkj@gmail.com','Pta-18092002','other','visitor','2025-05-28 16:47:00'),(5,'paco','paco@gmail.com','Pta-18092002','male','visitor','2025-05-28 19:47:25'),(6,'admin','admin@gmail.com','Pta-18092002','male','admin','2025-05-28 19:47:25'),(7,'prueba','prueba@gmail.com','Pta-18092002','male','visitor','2025-05-30 04:35:02'),(8,'UsuarioDonador','donador@gmail.com','Pta-18092002','female','visitor','2025-05-30 14:26:43'),(9,'pruebaRegistro','pruebaRegistro@gmail.com','Pta-18092002','male','visitor','2025-05-30 14:35:25'),(10,'prueba4','prueba4@gmail.com','Pta-18092002','male','visitor','2025-05-30 14:41:54'),(11,'prueba5','prueba5@gmail.com','Pta-18092002','male','visitor','2025-05-30 14:47:05'),(12,'Adoptante','adoptante@gmail.com','Pta-18092002','male','visitor','2025-05-30 14:55:51');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-30 20:50:12
